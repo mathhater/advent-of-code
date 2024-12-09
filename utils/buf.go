fb59a359d585a15a1ed2e68dfa3dd3b8d7806fcd
mathhater <aig0016@dgist.ac.kr>
2024-12-10 00:02:06 +0900

    LF CR/LF diff

--- utils/buf.go
+++ utils/buf.go
@@ -2,12 +2,16 @@ package utils
 
 import (
 	"bufio"
+	"runtime"
 	"strings"
 )
 
 func ReadLine(reader *bufio.Reader) (string, error) {
 	line, err := reader.ReadString('\n')
-	return strings.Replace(line, "\r\n", "", -1), err
+	if runtime.GOOS == "windows" {
+		return strings.Replace(line, "\r\n", "", -1), err
+	}
+	return strings.Replace(line, "\n", "", -1), err
 }
 
 func WriteLine(writer *bufio.Writer, str string) {
