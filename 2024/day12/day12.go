312b0c0c59fb0ac5857e5e926a21ac9eead6f92d
mathhater <aig0016@dgist.ac.kr>
2024-12-14 03:19:14 +0900

    fix day 12

--- 2024/day12/day12.go
+++ 2024/day12/day12.go
@@ -51,43 +51,40 @@ func getPrice2(g [][]byte, visit [][]bool, dir [][]int, y, x, H, W int) int {
 			edge[i][j] = make([]bool, W)
 		}
 	}
-	Q := []Point{{y, x}}
+	Q := list.New()
+	Q.PushBack(Point{y, x})
 	visit[y][x] = true
 	sides := 0
-	area := 1
-	for len(Q) > 0 {
-		var tmpQ []Point
-		for i := 0; i < len(Q); i++ {
-			p := Q[i]
-			for j := 0; j < len(dir); j++ {
-				ty := p.y + dir[j][0]
-				tx := p.x + dir[j][1]
-				if ty < 0 || ty >= H || tx < 0 || tx >= W || g[ty][tx] != g[y][x] {
-					edge[j][p.y][p.x] = true
-					sides++
-					rdy, rdx := dir[j][1], dir[j][0]
-					tty := p.y + rdy
-					ttx := p.x + rdx
-					
-					if 0 <= tty && tty < H && 0 <= ttx && ttx < W && edge[j][tty][ttx] {
-						sides--
-					}
-					tty = p.y + rdy * -1
-					ttx = p.x + rdx * -1
-					if 0 <= tty && tty < H && 0 <= ttx && ttx < W && edge[j][tty][ttx] {
-						sides--
-					}
-					continue
+	area := 0
+	for Q.Len() > 0 {
+		p := Q.Remove(Q.Front()).(Point)
+		area++
+		for j := 0; j < len(dir); j++ {
+			ty := p.y + dir[j][0]
+			tx := p.x + dir[j][1]
+			if ty < 0 || ty >= H || tx < 0 || tx >= W || g[ty][tx] != g[y][x] {
+				edge[j][p.y][p.x] = true
+				sides++
+				rdy, rdx := dir[j][1], dir[j][0]
+				tty := p.y + rdy
+				ttx := p.x + rdx
+				
+				if 0 <= tty && tty < H && 0 <= ttx && ttx < W && edge[j][tty][ttx] {
+					sides--
 				}
-				if visit[ty][tx] {
-					continue
+				tty = p.y + rdy * -1
+				ttx = p.x + rdx * -1
+				if 0 <= tty && tty < H && 0 <= ttx && ttx < W && edge[j][tty][ttx] {
+					sides--
 				}
-				tmpQ = append(tmpQ, Point{ty, tx})
-				visit[ty][tx] = true
-				area++
+				continue
 			}
+			if visit[ty][tx] {
+				continue
+			}
+			Q.PushBack(Point{ty, tx})
+			visit[ty][tx] = true
 		}
-		Q = tmpQ
 	}
 	return area * sides
 }
