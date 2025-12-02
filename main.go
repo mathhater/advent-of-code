ae1b534194e3ece3c1b093d4e9f3094918a1a51a
mathhater <aig0016@dgist.ac.kr>
2025-12-03 04:06:17 +0900

    add advent of code 2025 package

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	adventOfCode2024 "github.com/mathhater/advent-of-code/2024"
+	adventOfCode2025 "github.com/mathhater/advent-of-code/2025"
 )
 
 func main() {
 	adventOfCode2024.AdventOfCode2024()
+	adventOfCode2025.AdventOfCode2025()
 }
